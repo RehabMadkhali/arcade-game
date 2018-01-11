ccc49dce6bc348181a8c03e1d00a0ab9e061961e
iMishaDev <meenhye@hotmail.com>
2018-01-11 21:10:39 +0300

    l	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,120 +1,118 @@
-
-
 // Creating Class Enemy with all needed Functionalities.
-var Enemy = function(x,y) {
-    this.sprite = 'images/enemy-bug.png';
-    this.x = x;
-    this.y = y;
+var Enemy = function(x, y) {
+  this.sprite = 'images/enemy-bug.png';
+  this.x = x;
+  this.y = y;
 
-    this.speed = Math.floor(Math.random() * 300 + 100);
+  this.speed = Math.floor(Math.random() * 300 + 100);
 };
 
 // it increments the x position of the enemy until it reach the end of the canvas the it back again to the zero point.
 Enemy.prototype.update = function(dt) {
-    if(this.x <= 500)
-        this.x += this.speed * dt;
-     else
-        this.x = 0;
+  if (this.x <= 500)
+    this.x += this.speed * dt;
+  else
+    this.x = 0;
 };
 
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Creating Class Player with all needed Functionalities.
 // i needed alive and win pointers to check if the player really win or die. it's needed to print the message.
 var Player = function() {
-    this.sprite = 'images/char-horn-girl.png';
-    this.x = 200;
-    this.y = 440;
-    this.alive = true;
-    this.win = false;
+  this.sprite = 'images/char-horn-girl.png';
+  this.x = 200;
+  this.y = 440;
+  this.alive = true;
+  this.win = false;
 };
 
 
 // controll the movement of the player by clicking lift, righ,up and down by incrementing and decrementing the x , y that represents player position.
 Player.prototype.update = function() {
 
-var keyPressed = this.pressedKey;
+  var keyPressed = this.pressedKey;
 
   switch (keyPressed) {
     case 'up':
-      if(this.y > 0){
+      if (this.y > 0) {
         this.y -= 70;
       }
       break;
 
-      case 'down':
-      if(this.y < 400){
+    case 'down':
+      if (this.y < 400) {
         this.y += 70;
       }
       break;
 
-      case 'left':
-      if(this.x > 0){
+    case 'left':
+      if (this.x > 0) {
         this.x -= 100;
       }
       break;
 
-      case 'right':
-      if(this.x < 400){
+    case 'right':
+      if (this.x < 400) {
         this.x += 100;
       }
       break;
 
-      case 'enter':
+    case 'enter':
       reset();
       break;
-      default:
+    default:
 
-}
+  }
 
-// to stop looping through the same movement. we need it to move once.
-    this.pressedKey = null;
+  // to stop looping through the same movement. we need it to move once.
+  this.pressedKey = null;
 
-// player reaches the water!
-    if(this.y < 0){
-      this.win = true;
-      document.removeEventListener('keyup', handleKeyUpListenerDuringPlaying);
-      document.addEventListener('keyup', handleKeyUpListenerDuringEndOfGame);
-      this.reset();
-    }
+  // player reaches the water!
+  if (this.y < 0) {
+    this.win = true;
+    document.removeEventListener('keyup', handleKeyUpListenerDuringPlaying);
+    document.addEventListener('keyup', handleKeyUpListenerDuringEndOfGame);
+    this.reset();
+  }
 
-// we're gonna need a refrence to the player inside the clouser of the forEach. that's why i declare a global var to that function.
-    var player = this;
+  // we're gonna need a refrence to the player inside the clouser of the forEach. that's why i declare a global var to that function.
+  var player = this;
 
-    enemies.forEach(function(enemy) {
-      if ( player.x >= enemy.x - 30 && player.x <= enemy.x + 30) {
-        if ( player.y >= enemy.y - 30 && player.y <= enemy.y + 30) {
-          player.alive = false;
-          document.removeEventListener('keyup', handleKeyUpListenerDuringPlaying);
-          document.addEventListener('keyup', handleKeyUpListenerDuringEndOfGame);
-          player.reset();
-        }
+  enemies.forEach(function(enemy) {
+    if (player.x >= enemy.x - 30 && player.x <= enemy.x + 30) {
+      if (player.y >= enemy.y - 30 && player.y <= enemy.y + 30) {
+        player.alive = false;
+        document.removeEventListener('keyup', handleKeyUpListenerDuringPlaying);
+        document.addEventListener('keyup', handleKeyUpListenerDuringEndOfGame);
+        player.reset();
       }
-    });
+    }
+  });
 };
 
 
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 Player.prototype.handleInput = function(e) {
-    this.pressedKey = e;
+  this.pressedKey = e;
 };
 
 
 Player.prototype.reset = function() {
-   this.x = 200;
-   this.y = 440;
+  this.x = 200;
+  this.y = 440;
 };
 
 // it resets everything after winning or losing the player the game. it turns back everything to the default state.
-var reset = function () {
-    player.win = false;
-    player.alive = true;
-    document.addEventListener('keyup', handleKeyUpListenerDuringPlaying);
+var reset = function() {
+  player.win = false;
+  player.alive = true;
+  document.addEventListener('keyup', handleKeyUpListenerDuringPlaying);
 
 }
 
@@ -129,11 +127,11 @@ enemies.push(new Enemy(0, 220));
 
 function handleKeyUpListenerDuringPlaying(e) {
   var allowedKeys = {
-      13: 'enter',
-      37: 'left',
-      38: 'up',
-      39: 'right',
-      40: 'down'
+    13: 'enter',
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
   };
 
   player.handleInput(allowedKeys[e.keyCode]);
@@ -143,7 +141,7 @@ function handleKeyUpListenerDuringPlaying(e) {
 // except for 'enter' wich will get everything back to normal and the user can play again.
 function handleKeyUpListenerDuringEndOfGame(e) {
   var allowedKeys = {
-      13: 'enter',
+    13: 'enter',
   };
 
   player.handleInput(allowedKeys[e.keyCode]);
